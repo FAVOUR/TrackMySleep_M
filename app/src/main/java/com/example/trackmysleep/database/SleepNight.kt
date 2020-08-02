8f21b567c4bd76d79b4b69e0bc267d7e030db9d2
FAVOUR <truelyfavoured@gmail.com>
2020-08-02 19:10:57 +0100

    [DataBase setup] - Entity class fully setup.

--- app/src/main/java/com/example/trackmysleep/database/SleepNight.kt
+++ app/src/main/java/com/example/trackmysleep/database/SleepNight.kt
@@ -5,4 +5,16 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
- class SleepNight()
+ class SleepNight(
+ @PrimaryKey(autoGenerate = true)
+ val nightId:Long=0L,
+
+ @ColumnInfo(name="start_time_mill")
+ val startTimeMill:Long = System.currentTimeMillis(),
+
+ @ColumnInfo(name = "end_time_mill")
+ val endTimeMill:Long = startTimeMill,
+
+ @ColumnInfo(name = "quality_rating")
+ val sleepQuality:Int =-1
+)
