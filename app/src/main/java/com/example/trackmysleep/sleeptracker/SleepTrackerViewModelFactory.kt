6cb2781eb9376592e2bbd2b56aec238bdb37d90c
FAVOUR <truelyfavoured@gmail.com>
2020-08-03 23:22:33 +0100

    Created the SleepViewmodelFactory class

--- app/src/main/java/com/example/trackmysleep/sleeptracker/SleepTrackerViewModelFactory.kt
+++ app/src/main/java/com/example/trackmysleep/sleeptracker/SleepTrackerViewModelFactory.kt
@@ -1,4 +1,20 @@
 package com.example.trackmysleep.sleeptracker
 
-class SleepTrackerViewModelFactory {
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import com.example.trackmysleep.database.SleepDataBaseDoa
+
+class SleepTrackerViewModelFactory(val dataBaseDoa: SleepDataBaseDoa,val application: Application): ViewModelProvider.Factory{
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SleepTrackerViewModel::class.java)){
+
+            return SleepTrackerViewModel(dataBaseDoa,application) as T
+        }
+
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
