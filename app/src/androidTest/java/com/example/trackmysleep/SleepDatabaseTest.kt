185b4242fd54efc4b91cc19530ab9ed8ced6dc55
FAVOUR <truelyfavoured@gmail.com>
2020-08-05 22:01:23 +0100

    Fixed Added a test to the room database

--- app/src/androidTest/java/com/example/trackmysleep/SleepDatabaseTest.kt
+++ app/src/androidTest/java/com/example/trackmysleep/SleepDatabaseTest.kt
@@ -7,8 +7,11 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.trackmysleep.database.SleepDataBaseDoa
 import com.example.trackmysleep.database.SleepDatabase
+import com.example.trackmysleep.database.SleepNight
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -16,6 +19,7 @@ class SleepDatabaseTest  {
 
     lateinit var taskDoa:SleepDataBaseDoa
     lateinit var sleepDatabase:SleepDatabase
+    lateinit var sleepDao:SleepDataBaseDoa
 
     @Before
     fun create(){
@@ -23,12 +27,24 @@ class SleepDatabaseTest  {
         sleepDatabase= Room.databaseBuilder(context,SleepDatabase::class.java,"sleepDp")
                             .allowMainThreadQueries()
                             .build()
+
+        sleepDao=sleepDatabase.sleepDataBaseDoa
     }
 
 
     @After
     fun destroyDatabase(){
         sleepDatabase.close()
+    }
+
+
+    @Test
+    @Throws(Exception::class)
+    fun insertAndGetNight() {
+        val night = SleepNight()
+        sleepDao.insert(night)
+        val tonight = sleepDao.getToNight()
+        Assert.assertEquals(tonight?.sleepQuality, -1)
     }
 
 
