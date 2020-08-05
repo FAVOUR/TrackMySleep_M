14a8a810066792879edfc71988a8f8f58fc2947e
FAVOUR <truelyfavoured@gmail.com>
2020-08-05 21:00:18 +0100

    Setup the sleep Database test.

--- app/src/androidTest/java/com/example/trackmysleep/SleepDatabaseTest.kt
+++ app/src/androidTest/java/com/example/trackmysleep/SleepDatabaseTest.kt
@@ -1,10 +1,37 @@
 package com.example.trackmysleep
 
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.trackmysleep.database.SleepDataBaseDoa
+import com.example.trackmysleep.database.SleepDatabase
+import org.junit.After
+import org.junit.Before
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class SleepDatabaseTest  {
+
+    lateinit var taskDoa:SleepDataBaseDoa
+    lateinit var sleepDatabase:SleepDatabase
+
+    @Before
+    fun create(){
+        val context: Context = ApplicationProvider.getApplicationContext()
+        sleepDatabase= Room.databaseBuilder(context,SleepDatabase::class.java,"sleepDp")
+                            .allowMainThreadQueries()
+                            .build()
+
+
+    }
+
+    @After
+    fun destroyDatabase(){
+        sleepDatabase.close()
+    }
+
 
 
 }
