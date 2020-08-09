9dc59853fb136545cf9c5f10a434678637033552
FAVOUR <truelyfavoured@gmail.com>
2020-08-09 21:09:37 +0100

    Made the  Viewholder constructor  private and the setup of creation of the viewHolder public companion object to take the viewGroup as the parameter.

--- app/src/main/java/com/example/trackmysleep/sleeptracker/adapter/SleepNightAdapter.kt
+++ app/src/main/java/com/example/trackmysleep/sleeptracker/adapter/SleepNightAdapter.kt
@@ -18,11 +18,7 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
          notifyDataSetChanged()
      }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-
-        val view = inflater.inflate(R.layout.list_item_sleep_night,parent,false)
-
-          return (SleepNightAdapter.ViewHolder(view))
+        return ViewHolder.from(parent)
     }
 
     override fun getItemCount(): Int {
@@ -37,7 +33,7 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
 
 
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+     class  ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView){
         val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
         val quality: TextView = itemView.findViewById(R.id.quality_string)
         val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
@@ -60,6 +56,19 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
                 }
             )
         }
+
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val inflater = LayoutInflater.from(parent.context)
+
+                val view = inflater.inflate(R.layout.list_item_sleep_night, parent, false)
+
+                return (ViewHolder(view))
+            }
+        }
     }
+
+
 
 }
