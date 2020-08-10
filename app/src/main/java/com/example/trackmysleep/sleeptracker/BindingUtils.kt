6d17ffc3859e0e187aaa7dcf579da9d1e252f4a7
FAVOUR <truelyfavoured@gmail.com>
2020-08-10 23:26:56 +0100

    Created Adapter binding class

---
+++ app/src/main/java/com/example/trackmysleep/sleeptracker/BindingUtils.kt
@@ -0,0 +1,48 @@
+package com.example.trackmysleep.sleeptracker
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.trackmysleep.R
+import com.example.trackmysleep.convertDurationToFormatted
+import com.example.trackmysleep.convertNumericQualityToString
+import com.example.trackmysleep.database.SleepNight
+
+class BindingUtils {
+
+    @BindingAdapter("setSleepDurationFormatted")
+    fun TextView.setSleepDurationFormatted(item: SleepNight) {
+        text = convertDurationToFormatted(
+            item.startTimeMill,
+            item.endTimeMill,
+            context.resources
+        )
+
+    }
+
+
+    @BindingAdapter("sleepQualityString")
+    fun TextView.setSleepQualityString(item: SleepNight) {
+        text = convertNumericQualityToString(
+            item.sleepQuality,
+            context.resources
+        )
+    }
+
+
+
+    @BindingAdapter("sleepImage")
+    fun ImageView.setImage(item:SleepNight) {
+        setImageResource(
+            when (item.sleepQuality) {
+                0 -> R.drawable.ic_sleep_0
+                1 -> R.drawable.ic_sleep_1
+                2 -> R.drawable.ic_sleep_2
+                3 -> R.drawable.ic_sleep_3
+                4 -> R.drawable.ic_sleep_4
+                5 -> R.drawable.ic_sleep_5
+                else -> R.drawable.ic_sleep_active
+            }
+        )
+    }
+}
--- app/src/main/java/com/example/trackmysleep/sleeptracker/adapter/SleepNightAdapter.kt
+++ app/src/main/java/com/example/trackmysleep/sleeptracker/adapter/SleepNightAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -46,21 +47,7 @@ class SleepNightAdapter : ListAdapter<SleepNight,SleepNightAdapter.ViewHolder>(S
 
          fun bind(item: SleepNight) {
             val res = itemView.context.resources
-            sleepLength.text =
-                convertDurationToFormatted(item.startTimeMill, item.endTimeMill, res)
-            quality.text = convertNumericQualityToString(item.sleepQuality, res)
 
-            qualityImage.setImageResource(
-                when (item.sleepQuality) {
-                    0 -> R.drawable.ic_sleep_0
-                    1 -> R.drawable.ic_sleep_1
-                    2 -> R.drawable.ic_sleep_2
-                    3 -> R.drawable.ic_sleep_3
-                    4 -> R.drawable.ic_sleep_4
-                    5 -> R.drawable.ic_sleep_5
-                    else -> R.drawable.ic_sleep_active
-                }
-            )
         }
 
 
