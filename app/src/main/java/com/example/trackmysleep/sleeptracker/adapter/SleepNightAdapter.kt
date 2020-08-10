4304f2c4ff34038e9fc7b6ff0744b6d731db820e
FAVOUR <truelyfavoured@gmail.com>
2020-08-10 22:29:24 +0100

    Used Diff util to improve the performance of the list

--- app/src/main/java/com/example/trackmysleep/sleeptracker/adapter/SleepNightAdapter.kt
+++ app/src/main/java/com/example/trackmysleep/sleeptracker/adapter/SleepNightAdapter.kt
@@ -5,34 +5,45 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.trackmysleep.R
 import com.example.trackmysleep.convertDurationToFormatted
 import com.example.trackmysleep.convertNumericQualityToString
 import com.example.trackmysleep.database.SleepNight
 
-class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
-    var data = listOf<SleepNight>()
-     set(value) {
-         field =value
-         notifyDataSetChanged()
-     }
+class SleepNightAdapter : ListAdapter<SleepNight,SleepNightAdapter.ViewHolder>(SleepNightDiffUtilCallback()) {
+//    var data = listOf<SleepNight>()
+//     set(value) {
+//         field =value
+//         notifyDataSetChanged()
+//     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder.from(parent)
     }
 
-    override fun getItemCount(): Int {
-       return  data.size
-    }
+//    override fun getItemCount(): Int {
+//       return  data.size
+//    }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = data[position]
+        val item = getItem(position)
 
         holder.bind(item)
     }
 
 
+      class SleepNightDiffUtilCallback : DiffUtil.ItemCallback<SleepNight>(){
+          override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+              return  oldItem.nightId == newItem.nightId
+          }
 
+          override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+              return  oldItem == newItem
+          }
+
+      }
      class  ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView){
         val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
         val quality: TextView = itemView.findViewById(R.id.quality_string)
